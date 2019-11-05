87289684fab21cdfccc3fcae26661b1bfd18f83a
simmetric <github@simmetric.nl>
2019-11-05 14:58:58 +0100

    Removed Node stuff, not needed on .net core appsvc

--- index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var server = express();
-var options = {
-    index: 'index.html'
-};
-server.use('/', express.static(__dirname + '/', options));
-server.listen(process.env.PORT);
